dede8cbdc3adf210c0ae18423fb084800aa3e80f
Pablo <byscotexx@gmail.com>
2021-03-24 16:00:21 +0100

    Espere que este funcione

--- src/main/java/Herramienta/Gestor.java
+++ src/main/java/Herramienta/Gestor.java
@@ -1,4 +1,5 @@
 package Herramienta;
+//PORQUE NO FUNCIONA ESTO?
 
 import Persona.Persona;
 import Proyectos.Proyecto;
@@ -7,7 +8,7 @@ import Tarea.Tarea;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-public class Gestor {
+public class Gestor {//Este es el Gestor, que utilizamos como clase principal
 
     public static void main(String[] args) {
         for (int i = 0; i < 5 ; i++) {
